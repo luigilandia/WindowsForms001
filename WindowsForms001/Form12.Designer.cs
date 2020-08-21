054daab62575594be15e80e501e323a0e34b52ff
Luisa <luisainformat@gmail.com>
2020-08-21 11:05:08 +0200

    nuevo

--- WindowsForms001/Form12.Designer.cs
+++ WindowsForms001/Form12.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsForms001
 {
-    partial class Form11
+    partial class Form12
     {
         /// <summary>
         /// Required designer variable.
@@ -37,16 +37,16 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(34, 35);
+            this.dataGridView1.Location = new System.Drawing.Point(61, 26);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(530, 264);
+            this.dataGridView1.Size = new System.Drawing.Size(533, 229);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(97, 328);
+            this.button1.Location = new System.Drawing.Point(95, 302);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(165, 43);
+            this.button1.Size = new System.Drawing.Size(186, 62);
             this.button1.TabIndex = 1;
             this.button1.Text = "Aceptar";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,15 +54,15 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(324, 328);
+            this.button2.Location = new System.Drawing.Point(354, 302);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(163, 43);
+            this.button2.Size = new System.Drawing.Size(149, 62);
             this.button2.TabIndex = 2;
             this.button2.Text = "Borrar";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form11
+            // Form12
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -70,9 +70,9 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "Form11";
-            this.Text = "Form11";
-            this.Load += new System.EventHandler(this.Form11_Load);
+            this.Name = "Form12";
+            this.Text = "Form12";
+            this.Load += new System.EventHandler(this.Form12_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
--- WindowsForms001/Form12.cs
+++ WindowsForms001/Form12.cs
@@ -12,16 +12,17 @@ using System.Windows.Forms;
 
 namespace WindowsForms001
 {
-    public partial class Form11 : Form
+    public partial class Form12 : Form
     {
         DataSet midataset;
         SqlDataAdapter adaptador1 = new SqlDataAdapter();
-        public Form11()
+
+        public Form12()
         {
             InitializeComponent();
         }
 
-        private void Form11_Load(object sender, EventArgs e)
+        private void Form12_Load(object sender, EventArgs e)
         {
             midataset = new DataSet();
 
@@ -31,7 +32,7 @@ namespace WindowsForms001
             SqlConnection conexion = new SqlConnection(cadena);
             {
                 conexion.Open();
-                String sql = "select * from factura";
+                String sql = "select * from Personas";
                 SqlCommand comando = conexion.CreateCommand();
                 comando.CommandText = sql;
                 comando.CommandType = CommandType.Text;
@@ -39,55 +40,55 @@ namespace WindowsForms001
                 SqlCommand comandoInsertar = conexion.CreateCommand();
                 comandoInsertar.CommandType = CommandType.Text;
                 //consulta parametrizada
-                String sqlInsertar = "Insert into Factura (Numero, Concepto) values (" +
-                    "@Numero, @Concepto)";
+                String sqlInsertar = "Insert into Personas (DNI, Nombre, Apellidos) values (" +
+                    "@DNI, @Nombre, @Apellidos)";
                 comandoInsertar.CommandText = sqlInsertar;
-                comandoInsertar.Parameters.Add("@Numero", SqlDbType.Int, 10, "Numero");
-                comandoInsertar.Parameters.Add("@Concepto", SqlDbType.VarChar, 20, "Concepto");
+                comandoInsertar.Parameters.Add("@DNI", SqlDbType.VarChar, 10, "DNI");
+                comandoInsertar.Parameters.Add("@Nombre", SqlDbType.VarChar, 20, "Nombre");
+                comandoInsertar.Parameters.Add("@Apellidos", SqlDbType.VarChar, 50, "Apellidos");
                 //ésta es la clave primaria de la tabla con la cual insertaremos nuevos elementos
-                comandoInsertar.Parameters["@Numero"].SourceVersion = DataRowVersion.Original;
+                comandoInsertar.Parameters["@DNI"].SourceVersion = DataRowVersion.Original;
 
-                String sqlActualizar = "Update Factura set Concepto=@Concepto where Numero=@Numero";
+                String sqlActualizar = "Update Personas set Nombre=@Nombre where DNI=@DNI";
                 SqlCommand comandoActualizar = conexion.CreateCommand();
 
-                comandoActualizar.Parameters.Add("@Numero", SqlDbType.Int, 10);
-                comandoActualizar.Parameters.Add("@Concepto", SqlDbType.VarChar, 20);
-                comandoActualizar.Parameters["@Numero"].SourceVersion = DataRowVersion.Original;
+                comandoActualizar.Parameters.Add("@DNI", SqlDbType.VarChar, 10, "DNI");
+                comandoActualizar.Parameters.Add("@Nombre", SqlDbType.VarChar, 20, "Nombre");
+                comandoActualizar.Parameters.Add("@Apellidos", SqlDbType.VarChar, 50, "Apellidos");
+                comandoActualizar.Parameters["@DNI"].SourceVersion = DataRowVersion.Original;
                 comandoActualizar.CommandType = CommandType.Text;
                 comandoActualizar.CommandText = sqlActualizar;
 
-                string sqlBorrar = "delete from Factura where Numero=@Numero";
+                string sqlBorrar = "delete from Personas where DNI=@DNI";
                 SqlCommand comandoBorrar = conexion.CreateCommand();
                 comandoBorrar.CommandText = sqlBorrar;
                 comandoBorrar.CommandType = CommandType.Text;
-                comandoBorrar.Parameters.Add("@Numero", SqlDbType.Int, 10, "Numero");
-                comandoBorrar.Parameters["@Numero"].SourceVersion = DataRowVersion.Original;
-                
+                comandoBorrar.Parameters.Add("@DNI", SqlDbType.VarChar, 10, "DNI");
+                comandoBorrar.Parameters["@DNI"].SourceVersion = DataRowVersion.Original;
 
                 adaptador1.SelectCommand = comando;
                 adaptador1.InsertCommand = comandoInsertar;
                 adaptador1.UpdateCommand = comandoActualizar;
                 adaptador1.DeleteCommand = comandoBorrar;
 
-                adaptador1.Fill(midataset, "Factura");
+                adaptador1.Fill(midataset, "Personas");
 
 
                 dataGridView1.DataSource = midataset;
-                dataGridView1.DataMember = "Factura";
-
+                dataGridView1.DataMember = "Personas";
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            adaptador1.Update(midataset, "Factura");
+            adaptador1.Update(midataset, "Personas");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             DataGridViewRow fila = dataGridView1.CurrentRow;
             dataGridView1.Rows.Remove(fila);
-            adaptador1.Update(midataset, "Factura");
+            adaptador1.Update(midataset, "Personas");
         }
     }
 }

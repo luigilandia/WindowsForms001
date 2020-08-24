53efbcd5d162aab4755dfb1417157518ee207db7
Luisa <luisainformat@gmail.com>
2020-08-24 11:30:31 +0200

    nuevo

---
+++ ConsoleXML/Program6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleXML
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder cadena = new StringBuilder();
+            for (int i=0; i<100000; i++)
+            {
+                cadena.Append("hola");
+                cadena.Append(i);
+                cadena.Append("adios");
+                cadena.Append(i);
+                cadena.Append("hola2");
+            }
+            Console.WriteLine("terminó");
+            Console.ReadLine();
+        }
+    }
+}
